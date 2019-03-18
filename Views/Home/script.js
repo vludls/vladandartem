b15ab118fed619b4e76af0f150dd9f95db0dc957
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-03-18 17:11:00 +0300

    EShop

---
+++ Views/Home/script.js
@@ -0,0 +1,33 @@
+/*class Elem {
+	constructor(selector) {
+		this.elem = document.querySelectorAll(selector);
+	}
+
+	html (value) {
+		alert(this.elem.length);
+	}
+
+	
+}
+
+var elem = new Elem('p');
+elem.html('!');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
