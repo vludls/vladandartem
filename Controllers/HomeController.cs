a9c59938a00e6fc21be664f7c815fc11f144a9a5
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-03-20 09:33:57 +0300

    Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace vladandartem.Controllers
             return View();
         }
 
+        public IActionResult add()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
