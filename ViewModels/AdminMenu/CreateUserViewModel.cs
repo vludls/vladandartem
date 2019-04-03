5ceb8a891c5cd2b5392dda2af858fb90fdadd4f6
vludls <vladislav.mikkoev@yandex.ru>
2019-04-03 09:28:20 +0300

    Добавлена возможность добавлять\удалять\изменять пользователей

---
+++ ViewModels/AdminMenu/CreateUserViewModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace vladandartem.ViewModels.AdminMenu
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Введите Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Год рождения")]
+        public int Year { get; set; }
+ 
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
