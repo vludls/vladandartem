4cff8ce554ca9c2a7e64eb67b2842536bae0cb0e
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-04-12 12:41:07 +0300

    удаление секции аяксом

--- wwwroot/js/add-section.js
+++ wwwroot/js/add-section.js
@@ -6,13 +6,27 @@ $('.add-new-category').each(function() {
             type: "POST",
             data: form.serialize(),
             success: function (data) {
-                //var NameOfSection = JSON.parse(data);
+                var NameOfSection = JSON.parse(data);
                 alert(data);
-                /*$('.users').append('<li><p>' + NameOfSection["Name"] + '</p><div class="centering"><button type="submit" class="btn btn-danger delete-user" data-toggle="modal" data-route-id="' + NameOfSection["Id"] + '" data-target="#a'+ NameOfSection["Id"] +'">Удалить</button></div></li>');
-                $('#add-category').val('');
-                $('.centering > button').each(forAddCategory);*/ //Для модалки
+                $('.users').append('<li><p>' + NameOfSection["Name"] + '</p><div class="centering"><button type="submit" class="btn btn-danger delete-user" data-toggle="modal" data-route-id="' + NameOfSection["Id"] + '" data-target="#a'+ NameOfSection["Id"] +'">Удалить</button></div></li>');
+                $('.add-category').val('');
+                $('.centering > button').each(forAddCategory); //Для модалки
             }
         });
         e.preventDefault();
     });
+});
+
+$('.section-delete').submit(function (e) {
+    var del = $(this);
+    $.ajax({
+        url: "/AdminMenu/SectionDelete",
+        type: "POST",
+        data: form.serialize(),
+        success: function () {
+            var test = 'centering';
+            alert($('.' + test).attr('class'));
+        }
+    });
+    e.preventDefault();
 })
