5efe2aa9f99a6350c435a183c7c8dac3e39a6a45
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-04-22 21:53:32 +0300

    корзина на vue

--- wwwroot/js/cart-vue.js
+++ wwwroot/js/cart-vue.js
@@ -1,26 +1,20 @@
-new Vue ({
-    el: '#cart',
-    data: {
-        test: 'hello',
-        quantity: 1,
-        price: 0,
-        priceOfUnit: 0,
-        products: [],
-        ImgPath: ''
-    },
-    mounted: function () {
-        axios
-          .post('/PersonalArea/CartGetCartProducts')
-          .then(response => {
-            this.products = response.data;
-            this.ImgPath = response.data[0].Product.ImgPath
-            //this.ImgPath = response.data
-          });
-        },
-    watch: {
-        quantity: function() {
-            this.price = this.quantity * this.$refs.priceOfUnit.value
-        }
+new Vue({
+  el: '#cart',
+  data: {
+    products: [],
+    totalPrice: 0
+  },
+  mounted: function () {
+    axios
+      .post('/PersonalArea/CartGetCartProducts')
+      .then(response => {
+        this.products = response.data;
+      });
+  },
+  watch: {
+    quantity: function () {
+      this.price = this.quantity * this.$refs.priceOfUnit.value
     }
-    
+  }
+
 })
