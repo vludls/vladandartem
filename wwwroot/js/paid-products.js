94793c5990092ab00a6bf4d2c831717ea3d0329b
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-04-24 17:58:02 +0300

    заказы vue

--- wwwroot/js/paid-products.js
+++ wwwroot/js/paid-products.js
@@ -10,22 +10,60 @@
     })
 })*/
 //выкатывание продуктов
+Vue.directive('scroll', {
+    inserted: function (el, binding) {
+      let f = function (evt) {
+        if (binding.value(evt, el)) {
+          window.removeEventListener('scroll', f)
+        }
+      }
+      window.addEventListener('scroll', f)
+    }
+  });
 
 new Vue ({
     el: '#paid-products',
     data: {
-        orders: []
+        orders: [],
+        scroll: 0,
+        from: 0
     },
     mounted: function () {
         axios
             .post('/PersonalArea/GetPaidProducts', null, {
                 params: {
-                    start: 0
+                    start: this.from
                 } 
             })
             .then(response => {
                 this.orders = response.data;
                 
             });
+        this.scroll += 5000;
+        this.from += 20
+    },
+    methods: {
+        handleScroll: function () {
+            axios
+                .post('/PersonalArea/GetPaidProducts', null, {
+                    params: {
+                        start: this.from
+                    } 
+                })
+                .then(response => {
+                    //this.orders = response.data;
+                    for(var key in response.data) {
+                        this.orders.push(response.data[key]);
+                    }
+                    
+                });
+            this.scroll += 5000;
+            this.from += 20
+        },
+        scrolling: function () {
+            if (window.scrollY > this.scroll) {
+                this.handleScroll()
+            }
+        }
     }
 })
