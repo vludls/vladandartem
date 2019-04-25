387df72e9d357e09ca1d58e558249f1e138e8892
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-04-25 16:03:58 +0300

    разделы на vue

--- wwwroot/js/add-section.js
+++ wwwroot/js/add-section.js
@@ -1,39 +1,12 @@
-/*$('.add-new-category').each(function() {
-    $(this).submit(function (e) {
-        var form = $(this);
-        $.ajax({
-            url: "/AdminMenu/SectionAdd",
-            type: "POST",
-            data: form.serialize(),
-            success: function (data) {
-                var NameOfSection = JSON.parse(data);
-                $('.users').append('<li id="s' + NameOfSection["Id"] + '"><p>' + NameOfSection["Name"] + '</p><div class="centering"><button type="submit" class="btn btn-danger delete-user" data-toggle="modal" data-route-id="' + NameOfSection["Id"] + '" data-target="#a'+ NameOfSection["Id"] +'">Удалить</button></div></li>');
-                $('.add-category').val('');
-                $('.centering > button').each(forAddCategory); //Для модалки
-            }
-        });
-        e.preventDefault();
-    });
-});
-
-$('.section-delete').submit(function (e) {
-    var del = $(this);
-    $.ajax({
-        url: "/AdminMenu/SectionDelete",
-        type: "POST",
-        data: del.serialize(),
-        success: function (data) {
-            var DelSection = data;
-            $('#s' + DelSection).remove();
-            $('.close-modal').trigger('click');
-        }
-    });
-    e.preventDefault();
-})*/
 new Vue ({
     el: '#sections',
     data: {
-        sections: []
+        sections: [],
+        modalId: 'id',
+        sectionId: '',
+        sectionName: '',
+        index: '',
+        addSectionName: ''
     },
     mounted: function () {
         axios
@@ -42,4 +15,39 @@ new Vue ({
                 this.sections = response.data;
             });
     },
+    methods: {
+        activateModal: function (sectionId, sectionName, index) {
+            this.modalId = 'id';
+            this.modalId += sectionId;
+            this.sectionName = sectionName;
+            this.sectionId = sectionId;
+            this.index = index
+        },
+        deleteSection: function (event) {
+            const data = new FormData(document.querySelector('.section-delete'));
+            data.append('id', this.sectionId);
+            axios
+            .post('/AdminMenu/SectionDelete', data 
+            )
+            .then(
+                this.sections.splice(this.index, 1)
+            );
+            event.preventDefault();
+        },
+        closeModal: function () {
+            $('.close-modal').trigger('click');
+        },
+        addSection: function (event) {
+            const data = new FormData(document.querySelector('.add-new-category'));
+            data.append('sectionName', this.addSectionName);
+            axios
+            .post('/AdminMenu/SectionAdd', data
+            )
+            .then(response => {
+                this.sections.push(response.data);
+                this.addSectionName = ''
+            });
+            event.preventDefault();
+        }
+    }
 })
