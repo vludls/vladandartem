34f73f9362baba77b80d341ef69286a5c8821367
ArtyomKobyatskiy <artyom0395@gmail.com>
2019-04-25 18:01:21 +0300

    категории

--- wwwroot/js/add-category.js
+++ wwwroot/js/add-category.js
@@ -18,13 +18,53 @@
 new Vue ({
     el: '#categories',
     data: {
-
+        categories: [],
+        modalId: 'id',
+        categoryId: '',
+        categoryName: '',
+        index: '',
+        addCategoryName: ''
     },
     mounted: function () {
         axios
             .post('/AdminMenu/Categories/Api/Get')
             .then(response => {
-                this.sections = response.data;
+                this.categories = response.data;
             });
     },
+    methods: {
+        activateModal: function (categoryId, categoryName, index) {
+            this.modalId = 'id';
+            this.modalId += categoryId;
+            this.categoryName = categoryName;
+            this.categoryId = categoryId;
+            this.index = index
+        },
+        deleteCategory: function (event) {
+            const data = new FormData(document.querySelector('.delete-category'));
+            data.append('categoryId', this.categoryId);
+            axios
+            .post('/AdminMenu/Categories/Api/Delete', data
+            )
+            .then(
+                this.categories.Categories[0].splice(this.index, 1)
+            );
+            event.preventDefault();
+        },
+        closeModal: function () {
+            $('.close-modal').trigger('click');
+        },
+        addCategory: function (event) {
+            const data = new FormData(document.querySelector('.add-new-category'));
+            data.append('sectionName', this.addCategoryName);
+            axios
+            .post('/AdminMenu/Categories/Api/Add', data
+            )
+            .then(response => {
+                this.categories.push(response.data);
+                this.addCategoryName = ''
+            });
+            event.preventDefault();
+        }
+    }
 })
