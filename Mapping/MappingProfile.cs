32ff7dd5a0079d388fcc12241ad2c49e25ec986a
vludls <vladislav.mikkoev@yandex.ru>
2019-05-07 16:01:52 +0300

    Должно работать

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -14,17 +14,21 @@ namespace vladandartem.Mapping
     {
         public MappingProfile()
         {
-            CreateMap<User, UserAvailableInfoReturn>();
-            CreateMap<UserSaveModel, User>();
+            CreateMap<User, User>().ForMember(newUser => newUser.Id, opt => opt.MapFrom(oldUser => oldUser.Id))
+                .ForMember(newUser => newUser.Email, opt => opt.MapFrom(oldUser => oldUser.Email))
+                .ForMember(newUser => newUser.Year, opt => opt.MapFrom(oldUser => oldUser.Year))
+                .ForAllOtherMembers(opt => opt.Ignore());
             CreateMap<Product, Product>();
             CreateMap<ProductDetailField, ProductDetailField>().ForMember(p => p.Product, opt => opt.UseDestinationValue());
             CreateMap<Definition, Definition>().ForMember(p => p.DetailField, opt => opt.UseDestinationValue());
             CreateMap<Order, Order>().ForMember(p => p.User, opt => opt.UseDestinationValue());
             CreateMap<CartProduct, CartProduct>().ForMember(p => p.Order, opt => opt.UseDestinationValue())
                 .ForMember(p => p.Cart, opt => opt.UseDestinationValue());
+
+            CreateMap<UserSaveModel, User>();
             CreateMap<CreateUserViewModel, User>();
             CreateMap<Product, AnalyticsLoadProductsOfChoosedCategoryViewModel>();
-            CreateMap<ProductDetailField, ProductDetailFieldAvailableInfoReturn>();
+            //CreateMap<ProductDetailField, ProductDetailFieldAvailableInfoReturn>();
         }
     }
 }
